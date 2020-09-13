4a7992bd0cdc45d6f7c14240b57f366cbf031be2
Jamie Dela Cruz <jamiedc@bu.edu>
2020-09-13 10:32:51 -0400

    Squashing original commit with resolving comments

--- 01-python/safety/firestation.py
+++ 01-python/safety/firestation.py
@@ -1,3 +1,32 @@
-def draw_firestation():
-    print("fire station not found")
+def draw_firestation(trucks=1):
+    if trucks < 0:
+      print("Uh oh, you can't have negative trucks!")
+      return
+    extra_trucks = trucks-1
+    print("      ________    ")
+    print("     /        \   ")
+    print("    /          \  ")
+    print("   /            \ ")
+    print("  /______________\ ")
+    print("  |              | ")
+    print("  | Volunteer    | ")
+    print("  | Fire         | ")
+    print("  | Department   | ")
+    if trucks == 0:
+      print("  |              | ")
+      print("  |              | ") 
+      print("  |              | ") 
+      print("  |              | ") 
+      print("  |              | ")
+      print("  |              | ") 
+      print("  |______________| ") 
+    else:
+        print("  |              |"+ extra_trucks*"______________")
+        print("  |              " + extra_trucks*"              " + "| ")
+        print("  |   _n____n_   " + extra_trucks*"   _n____n_   " + "| ")
+        print("  |   |______|   " + extra_trucks*"   |______|   " + "| ")
+        print("  |   |      |   " + extra_trucks*"   |      |   " + "| ")
+        print("  |   |      |   " + extra_trucks*"   |      |   " + "| ")
+        print("  |   |_|__|_|   " + extra_trucks*"   |_|__|_|   " + "| ")
+        print("  |____U____U____" + extra_trucks*"____U____U____" + "| ")
     return
